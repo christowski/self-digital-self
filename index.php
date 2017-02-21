ffde163a311b6ddfce17395f27b2072908d71471
christowski <mail@christowski.de>
2017-02-21 11:53:54 +0100

    added ALL the things

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+<meta name="description" content="" />
+<meta name="author" content="Christoph Rauscher" />
+<link rel="author" type="text/plain" href="humans.txt" />
+<link rel="shortcut icon" href="media/favicon.ico" />
+<title>Self / Digital Self — Bachelorarbeit von Christoph Rauscher</title>
+
+<!--[if IE]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<!-- STYLESHEETS -->
+<link rel="stylesheet" type="text/css" href="css/reset.css">
+<link rel="stylesheet" type="text/css" href="css/custom.css">
+
+<!-- JAVSCRIPT -->
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<link href='http://fonts.googleapis.com/css?family=Fjalla+One|Noticia+Text:400,400italic' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+
+<script>
+    $(document).ready(function () {
+      $('html').hide();
+      $('html').fadeIn(1000);
+    });
+</script>
+
+</head>
+
+<body>
+    
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/de_DE/all.js#xfbml=1";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+    
+    <div id="container">
+        <header>
+            <figure>
+                <img src="media/bg_image.gif" alt="Self / Digital Self">
+                <h1>Self / Digital Self</h1>
+            </figure>
+        </header>
+        
+        <div id="content">
+            
+            <p class="intro">Die Selbstwahrnehmung der eigenen Identität im digitalen Zeitalter hat sich verschoben. Im heutigen Mischmasch zwischen Analogität und Digitalität ist eine Neuorientierung der Selbstauslegung notwenig.</p>
+            <hr />
+            <p>Hierzu habe ich im Rahmen meiner Bachelorarbeit sechs Interviews mit jungen Menschen über ihre Identitätswahrnehmung, über Digitalisierung und Sicherheit, sowie über Selbstprojektion gesprochen.</p>
+            <p>
+                <div>
+                    <a class="cta" href="thesis.php">Bachelorarbeit lesen</a>
+                </div>
+                <div>
+                    <a class="cta_2" href="media/christophrauscher_selfdigitalself.epub">ePub Download</a>
+                </div>
+                <div>
+                    <a class="cta" href="media/christophrauscher_selfdigitalself.pdf" target="_blank">PDF herunterladen</a>
+                </div>
+            </p>
+            <hr />
+            <p>Erste Gedanken zum Thema habe ich bei einem <a href="http://up.front.ug/" target="_blank">up.front</a> Panel im Sommer 2012 <a href="https://speakerdeck.com/christowski/digital-self-at-up-dot-front-number-29" target="_blank">vorgestellt</a>. Die Thematik erhält überall im Alltag Aufmerksamkeit, die dokumentiert werden will: im <a href="http://selfdigitalself.tumblr.com">Produktionsblog</a> und via <a href="http://selfdigitalself.tumblr.com/submit/">Tumblr Submit</a>.</p>
+            <p class="small">Die HTML-Version enthält alle nötigen Fußnoten, allerdings nicht die Transkripte der Interviews. Sie sind in der ePub- und in der PDF-Datei enthalten. Über Feedback und Denkanstöße freue ich mich: <script type="text/javascript">document.write(
+"<n uers=\"znvygb:znvy\100puevfgbcuenhfpure\056qr\">znvy &nryvt;gg puevfgbcuenhfpure\056qr<\057n>".replace(/[a-zA-Z]/g, function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);}));
+</script>.</p>
+            <p>
+                <div class="fb-like" data-href="http://christophrauscher.de/selfdigitalself/" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false" data-action="recommend"></div>
+                
+                <!-- <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://christophrauscher.de/selfdigitalself/" data-via="christowski" data-lang="de">Twittern</a>
+                    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script> -->
+            </p>
+            
+            <footer>
+                <hr />
+                <p><a href="http://christophrauscher.de">Christoph Rauscher</a> &copy; <?php echo date("Y"); ?>.</p>
+            </footer>
+        </div> <!-- // content -->
+        
+    </div> <!-- // container -->
+    
+    <script type="text/javascript">
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-6347422-4']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+    </script>
+    
+</body>
+
+</html>
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Self / Digital Self
+This website gives access to my 2013 bachelor thesis about digital self-reflection and -awareness of millennials. It’s written in German, but the [production blog](https://www.selfdigitalself.tumblr.com) is English.
+
+The site contains a landing page with links to the thesis, a pdf and an e-book download. It’s a simple, static html page with some php for dates and stuff. Nothing fancy!
